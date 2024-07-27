34521c681034d92c3fb8f7089dcc91d85a14059d
Nagendran6M <nagendran170302@gmail.com>
2024-07-27 15:26:50 +0530

    Adding

---
+++ dsa.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+int  add(int a,int b){
+    int c=a+b;
+    return c;
+}
+int main(){
+    int Add=add(12,45);
+    cout<<Add;
+}
